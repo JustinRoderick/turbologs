4c18922307a7ff000dbf92c9dca1b86febe532d8
JustinRoderick <justinroderick258@gmail.com>
2026-03-03 22:38:17 -0500

    configure google auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -9,27 +9,29 @@ import type { DataModel } from "./_generated/dataModel";
 
 const siteUrl = process.env.SITE_URL!;
 
-// The component client has methods needed for integrating Convex with Better Auth,
-// as well as helper methods for general use.
 export const authComponent = createClient<DataModel>(components.betterAuth);
 export const createAuth = (ctx: GenericCtx<DataModel>) => {
   return betterAuth({
     baseURL: siteUrl,
+    socialProviders: {
+      google: { 
+        prompt: "select_account", 
+          clientId: process.env.GOOGLE_CLIENT_ID as string, 
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+      }, 
+  },
     database: authComponent.adapter(ctx),
-    // Configure simple, non-verified email/password to get started
     emailAndPassword: {
       enabled: true,
       requireEmailVerification: false,
     },
     plugins: [
-      // The Convex plugin is required for Convex compatibility
       convex({ authConfig }),
     ],
   });
 };
 
-// Example function for getting the current user
-// Feel free to edit, omit, etc.
+// delete later
 export const getCurrentUser = query({
   args: {},
   handler: async (ctx) => {
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,3 +4,11 @@ import { convexClient } from "@convex-dev/better-auth/client/plugins";
 export const authClient = createAuthClient({
   plugins: [convexClient()],
 });
+
+
+// Call in client
+const signIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+  });
+};
