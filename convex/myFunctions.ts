797c9c6630bda3b8d2332e44f966a6033744c6a1
JustinRoderick <justinroderick258@gmail.com>
2026-03-04 13:26:00 -0500

    .

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -1,3 +1,4 @@
+/*
 import { v } from "convex/values";
 import { action, mutation, query } from "./_generated/server";
 import { api } from "./_generated/api";
@@ -75,3 +76,4 @@ export const myAction = action({
     });
   },
 });
+*/
