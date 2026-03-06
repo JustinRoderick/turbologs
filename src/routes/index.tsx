3e7e9ec5daea9c3f1ba57711d6f19969b45d0f96
JustinRoderick <justinroderick258@gmail.com>
2026-03-05 23:24:00 -0500

    basic hero section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
-import { Link, createFileRoute } from "@tanstack/react-router";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
 import { convexQuery } from "@convex-dev/react-query";
 import { api } from "../../convex/_generated/api";
+import { Home } from "./-home";
 
 export const Route = createFileRoute("/")({
   component: Home,
+  /*
   loader: async ({ context }) => {
     await Promise.all([
       context.queryClient.ensureQueryData(
@@ -13,18 +14,5 @@ export const Route = createFileRoute("/")({
       // Load multiple queries in parallel if needed
     ]);
   },
+  */
 });
-
-function Home() {
-  const { data: currentUser } = useSuspenseQuery(
-    convexQuery(api.auth.getCurrentUser, {}),
-  );
-
-  return (
-    <main className="p-8 flex flex-col gap-16">
-      <h1 className="text-4xl font-bold text-center">
-        Convex + Tanstack Start
-      </h1>
-    </main>
-  );
-}
