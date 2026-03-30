5968bb6b90ed9ae6264c646ae3d80513c74074eb
JustinRoderick <justinroderick258@gmail.com>
2026-03-29 21:57:10 -0400

    build error

--- convex/env.ts
+++ convex/env.ts
@@ -1,5 +1,6 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+
 export const env = createEnv({
   server: {
     BETTER_AUTH_SECRET: z.string().min(32),
@@ -7,7 +8,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     STRIPE_SECRET_KEY: z.string().startsWith("sk_"),
-    STRIPE_WEBHOOK_SECRET: z.string().startsWith("whsec_"),
+    // STRIPE_WEBHOOK_SECRET: z.string().startsWith("whsec_"),w
   },
   runtimeEnvStrict: {
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
@@ -15,7 +16,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
   },
   emptyStringAsUndefined: true,
 });
