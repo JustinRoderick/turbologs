de0059818a8b40cda83c6937861b8d13c57bd27b
JustinRoderick <justinroderick258@gmail.com>
2026-05-03 23:37:13 -0400

    .

--- src/routes/auth/sign-in.tsx
+++ src/routes/auth/sign-in.tsx
@@ -2,5 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import SignInScreen from "./-sign-in";
 
 export const Route = createFileRoute("/auth/sign-in")({
+  validateSearch: (raw: Record<string, unknown>) => ({
+    redirect: typeof raw.redirect === "string" ? raw.redirect : undefined,
+  }),
   component: SignInScreen,
 });
